c49bda2bc390608caf40282d9cf8649860e1a982
Lukas Ruge <lukeslog@googlemail.com>
2016-05-05 14:49:37 +0200

    increase pool size for pg

--- server/persistence/persistence.js
+++ server/persistence/persistence.js
@@ -2,6 +2,8 @@ var pg = require('pg');
 
 var connectionString = process.env.ICEBOX_DB_URL || 'postgres://iceboxuser:testForIce@localhost:5432/icobox';
 
+pg.defaults.poolSize = 20;
+
 exports.connect = function(callback) {
   pg.connect(connectionString, callback);
 };
